7c5b82242a303e2211063a473df01c668a9947a6
JF Lemay <94007934+Jeff42820@users.noreply.github.com>
2022-07-07 17:12:31 +0200

    update copyright license & project name

--- src/upload.php
+++ src/upload.php
@@ -5,7 +5,7 @@ $html_header = <<<EOLONGTEXT
 
     =======================================
     ====
-    ====  This is the project yapfu 1phpfile 
+    ====  This is the project uploads-yapfu
     ====
     ====  Yet Another Php File Uploader (all-in-one-php-file)
     ====
@@ -13,15 +13,36 @@ $html_header = <<<EOLONGTEXT
     ====
     ====  2022-07-07
     ====
-    ====  v 0.912
+    ====  v 0.913
     ====
     ====
     ====
     ====
-    ==== (c) 2022 JF Lemay (FR) 
-    ==== Web2.0   all-in-one-php-file
-    ==== all right reserved for the moment
-    ==== expect a GNU LGPL or eqv soon
+    ==== (c) 2022 JF Lemay (FR) (contact via github Jeff42820)
+    ====
+        MIT License
+
+        Copyright (c) 2022 JF Lemay
+
+        Permission is hereby granted, free of charge, to any person obtaining a copy
+        of this software and associated documentation files (the "Software"), to deal
+        in the Software without restriction, including without limitation the rights
+        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        copies of the Software, and to permit persons to whom the Software is
+        furnished to do so, subject to the following conditions:
+
+        The above copyright notice and this permission notice shall be included in all
+        copies or substantial portions of the Software.
+
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        SOFTWARE.
+    ====
+    ====
     =======================================
 
 -->
